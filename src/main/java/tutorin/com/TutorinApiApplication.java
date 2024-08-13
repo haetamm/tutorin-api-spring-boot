0ed4f97ca84d0b8102e51c375f925fd3e79033d0
Thaetami <tatanghaetami.97@gmail.com>
2024-08-13 13:05:14 +0700

    first init

---
+++ src/main/java/tutorin/com/TutorinApiApplication.java
@@ -0,0 +1,13 @@
+package tutorin.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorinApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorinApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tutorin/com/TutorinApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tutorin.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorinApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
